dbfe63ff232da3b4f437ba33c559693d770b9add
sajib <sajibsaj66@gmail.com>
2023-10-30 10:08:26 +0600

    bd_payment_ssl_com

---
+++ android/app/src/main/kotlin/com/example/bd_payment_ssl_com/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bd_payment_ssl_com
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/my_home_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sslcommerz/model/SSLCSdkType.dart';
+import 'package:flutter_sslcommerz/model/SSLCTransactionInfoModel.dart';
+import 'package:flutter_sslcommerz/model/SSLCommerzInitialization.dart';
+import 'package:flutter_sslcommerz/model/SSLCurrencyType.dart';
+import 'package:flutter_sslcommerz/sslcommerz.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        centerTitle: true,
+        title: const Text('Payment Gateway',style: TextStyle(color: Colors.white),),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            sslCommerzGeneralCall();
+          },
+          child: const Text('Pay now'),
+        ),
+      ),
+    );
+  }
+
+  Future<void> sslCommerzGeneralCall() async {
+    Sslcommerz sslcommerz = Sslcommerz(
+      initializer: SSLCommerzInitialization(
+        //Use the ipn if you have valid one, or it will fail the transaction.
+        // ipn_url: "https://smartbd.com",
+        //multi_card_name: "visa,master,bkash",
+        currency: SSLCurrencyType.BDT,
+        product_category: "Over Taking Case",
+        sdkType: SSLCSdkType.TESTBOX,
+        store_id: 'smart64bd39c20d040',
+        store_passwd: 'smart64bd39c20d040@ssl',
+        total_amount: 700.0,
+        tran_id: "1231321321321312",
+      ),
+    );
+    try {
+      SSLCTransactionInfoModel result = await sslcommerz.payNow();
+
+      if (result.status!.toLowerCase() == "failed") {
+        Fluttertoast.showToast(
+          msg: "Transaction is Failed....",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0,
+        );
+      } else if (result.status!.toLowerCase() == "closed") {
+        Fluttertoast.showToast(
+          msg: "Transaction closed by User",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0,
+        );
+      } else if (result.status!.toLowerCase() == "valid") {
+        Fluttertoast.showToast(
+            msg: "Transaction is ${result.status} and Amount is ${result.amount}",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.black,
+            textColor: Colors.white,
+            fontSize: 16.0);
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
+
+}
